d339380a12b5d21e2fcea2845fd6ccf75c84581e
FranSar5868 <fsarrias@coit.es>
2023-04-23 18:36:33 +0200

    added basic structure

---
+++ script.js
@@ -0,0 +1,26 @@
+// var texto = document.getElementById('title');
+// console.log(texto);
+
+// var a = +prompt ('Entra el valor de a');
+// var b = parseInt(prompt ('Entra el valor de b'));
+
+function sumar () {
+    let suma = a + b;
+    alert("la suma es de " + suma +"\n");
+    return suma
+}
+
+
+// if ( sumar() >=15 )
+// {
+//     console.log('la suma de ' + a + ' + ' + b +' es igual o mayor que 15' + '\n');
+// } else {
+//     console.log('la suma de ' + a + ' + ' + b +' es inferior a 15' + '\n');
+// }
+
+
+// ( sumar() >=15 ) ? alert('la suma de ' + a + ' + ' + b +' es igual o mayor que 15 😂 ' +'\n') : alert('la suma de ' + a + ' + ' + b +' es inferior a 15 ' + '\n');
+
+let c = 5;
+let d = 10;
+console.log(`La suma de ${c} y ${d} es ${c+d}`);
